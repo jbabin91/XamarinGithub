cf98c63bc977f5dfa0cdf40f2e7b1f848e0b899c
Jace Babin <jbabin91@gmail.com>
2020-01-16 11:20:22 -0700

    Start of project to try Github Actions
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ XamarinGithub/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinGithub.Services;
+using XamarinGithub.Views;
+
+namespace XamarinGithub
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
